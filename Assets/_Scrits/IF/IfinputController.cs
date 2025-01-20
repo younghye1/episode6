027987eca816d3a9b4a315bc63f8cf1be52ba1d9
younghye1 <265o21247050@gmail.com>
2025-01-20 09:42:57 +0900

    0.0.4]edit : //목적: 문자는 패스, 숫자만 입력 후 전달

--- Assets/_Scrits/IF/IfinputController.cs
+++ Assets/_Scrits/IF/IfinputController.cs
@@ -15,28 +15,28 @@ public class IfinputController : MonoBehaviour
 
         //목적: 문자는 패스, 숫자만 입력 후 전달
         //Numfilter
-    output.text = NumFilter ( input.text);
+        output.text = NumFilter(input.text);
     }
 
     //입력: 문자 (string) -> 출력:숫자
     private int _temp;
-    string NumFilter( string s)
+    string NumFilter(string s)
     {
-        
-       //return int.Parse(s).ToString();
-       //return $"{int.Parse (s)}";
 
-       //참 일 경우 실행
-       if (int.TryParse(s, out int o))
-       {
-        _temp = o;
-        return o .ToString();
-       }
+        //return int.Parse(s).ToString();
+        //return $"{int.Parse (s)}";
 
-       //거짓으로 처리
-       else
-       {
-        return _temp.ToString();
-       }
+        //참 일 경우 실행
+        if (int.TryParse(s, out int o))
+        {
+            _temp = o;
+            return o.ToString();
+        }
+
+        //거짓으로 처리
+        else
+        {
+            return _temp.ToString();
+        }
     }
 }
